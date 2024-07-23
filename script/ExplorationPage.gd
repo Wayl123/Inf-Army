75b7cf26485297486ce0e08bedd4fb805a8e4851
Wayne Lee <waynel1996weiyang@gmail.com>
2024-07-23 15:57:23 -0700

    Exploration stat to display team size and current exploration power, also set up exploration area node

--- script/ExplorationPage.gd
+++ script/ExplorationPage.gd
@@ -2,11 +2,21 @@ extends PanelContainer
 
 @onready var unitInventory = get_tree().get_first_node_in_group("UnitInventory")
 
-@onready var test = %Test
+@onready var explorationStat = %ExplorationStat
+
+var teamSize = [0, 0]
 
 func _ready() -> void:
-	test.connect("pressed", Callable(self, "_test"))
-
-func _test():
-	var power = unitInventory.get_power_by_amount([1, 10])
-	test.text = str(power)
+	update_exploration_stat([1, 10])
+	
+func update_exploration_stat(pTeamSize : Array) -> void:
+	if pTeamSize[0] != teamSize[0]:
+		teamSize[0] = pTeamSize[0]
+		explorationStat.update_hero_cap(teamSize[0])
+	if pTeamSize[1] != teamSize[1]:
+		teamSize[1] = pTeamSize[1]
+		explorationStat.update_normal_unit_cap(teamSize[1])
+	update_exploration_power()
+	
+func update_exploration_power() -> void:
+	explorationStat.update_exploration_power(unitInventory.get_power_by_amount(teamSize))
